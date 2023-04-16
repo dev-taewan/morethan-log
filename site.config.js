b481702de8c3baa786ea52ec43af86d5b4fd330c
taewan kim <32315877+dev-taewan@users.noreply.github.com>
2023-04-16 18:29:31 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "taewan-kim",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "x86,arm Application Developer",
+    bio: "I want to become a developer who improves people's discomfort",
+    email: "root0607@gmail.com",
+    linkedin: "",
+    github: "https://github.com/dev-taewan",
     instagram: "",
   },
   projects: [
